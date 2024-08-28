5122dffe5ff597cc0c7f335cd17073e113b64f78
arifbaust10 <arifbaust10@gmail.com>
2024-08-28 15:43:16 +0600

    second commit

---
+++ app/src/main/java/com/example/attandanceapp/ClassItem.java
@@ -0,0 +1,27 @@
+package com.example.attandanceapp;
+
+public class ClassItem {
+    String className;
+    String subjeetName;
+
+    public ClassItem(String className, String subjeetName) {
+        this.className = className;
+        this.subjeetName = subjeetName;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getSubjeetName() {
+        return subjeetName;
+    }
+
+    public void setSubjeetName(String subjeetName) {
+        this.subjeetName = subjeetName;
+    }
+}
--- app/src/main/java/com/example/attandanceapp/MainActivity.java
+++ app/src/main/java/com/example/attandanceapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.attandanceapp;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
@@ -10,12 +12,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton fab;
+    RecyclerView recyclerView;
+
+    ClassAdapter classAdapter;
+    RecyclerView.LayoutManager layoutManager;
+
+    ArrayList<ClassItem> classItems = new ArrayList<>();
+
+    EditText class_edt;
+    EditText subject_edt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +38,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         fab = findViewById(R.id.fab_main);
-
         fab.setOnClickListener(v-> showDialog());
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        classAdapter = new ClassAdapter(this, classItems);
+        recyclerView.setAdapter(classAdapter);
 
     }
 
@@ -33,7 +56,31 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         View view = LayoutInflater.from(this).inflate(R.layout.class_dialog, null);
         builder.setView(view);
-        builder.create().show();
+        AlertDialog dialog =  builder.create();
+        dialog.show();
 
+        class_edt =view.findViewById(R.id.class_edt);
+        subject_edt = view.findViewById(R.id.subject_edt);
+
+        Button cancle = view.findViewById(R.id.cancel_btn);
+        Button add = view.findViewById(R.id.add_btn);
+
+        cancle.setOnClickListener(v-> dialog.dismiss());
+
+        add.setOnClickListener(v-> {
+            addClass();
+            dialog.dismiss();
+        });
+
+
+
+    }
+
+    private void addClass() {
+        String className = class_edt.getText().toString();
+        String subjectName = subject_edt.getText().toString();
+
+        classItems.add(new ClassItem(className, subjectName));
+        classAdapter.notifyDataSetChanged();
     }
 }
