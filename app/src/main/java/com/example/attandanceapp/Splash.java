590ea5c077d8f79b6fa5222735bf870b91324f82
arifbaust10 <arifbaust10@gmail.com>
2024-11-11 20:39:48 +0600

    13 commit(add splash and icon)

---
+++ app/src/main/java/com/example/attandanceapp/Splash.java
@@ -0,0 +1,38 @@
+package com.example.attandanceapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.WindowManager;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // For full screen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        // For Hide Action Bar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        // For Splash Screen
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Code here
+                Intent myIntent = new Intent(Splash.this, MainActivity.class);
+                startActivity(myIntent);
+                finish();
+            }
+        }, 1500);
+    }
+}
